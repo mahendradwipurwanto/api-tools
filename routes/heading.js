ea70f8120d53b784380ca43e2a51378df600ef9a
mahendradwipurwanto <mahendradwipurwanto@gmail.com>
2024-09-19 15:29:07 +0700

    remove puppeter

--- routes/heading.js
+++ routes/heading.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var axios = require('axios');
 var cheerio = require('cheerio');
-var puppeteer = require('puppeteer');
 var {buildHeadingStructure, restructureHeadingsDynamic, countHeadings, injectFontSize} = require('../libs/heading');
 
 /* GET return a JSON response */
@@ -13,13 +12,6 @@ router.get('/', async function (req, res, next) {
         if (!url) {
             return res.status(400).json({status: 'error', message: 'URL parameter is required'});
         }
-
-        // Launch Puppeteer
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
-
-        // Navigate to the URL
-        await page.goto(url, { waitUntil: 'networkidle2' });
 
         // Fetch the page content
         const {data: html} = await axios.get(url);
@@ -35,11 +27,6 @@ router.get('/', async function (req, res, next) {
 
         // restructure
         structure = restructureHeadingsDynamic(structure);
-
-        // Inject font size into the structure
-        structure = await injectFontSize(page, structure);
-
-        await browser.close(); // Close Puppeteer browser instance
 
         res.json({
             status: 'ok',
